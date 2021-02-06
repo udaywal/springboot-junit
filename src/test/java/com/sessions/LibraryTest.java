395c79149b3b0e6e50a983de3369a5ee7277c428
Vaibhav Udaywal <vaibhav.udaywal@otssolutions.com>
2021-02-06 20:46:05 +0530

    AAA principle pseudo code  added.

--- src/test/java/com/sessions/LibraryTest.java
+++ src/test/java/com/sessions/LibraryTest.java
@@ -14,36 +14,36 @@ public class LibraryTest {
 
     Library library = new Library();
 
-    public int totalBooksInLibrary () {
+    public int totalBooksInLibrary() {
         return library.getBooks().size();
     }
 
     /* It will trigger before all tests */
     @BeforeAll
-    public static void beforeAll () {
+    public static void beforeAll() {
         System.out.println("beforeAll");
     }
 
     /* It will trigger after all tests */
     @AfterAll
-    public static void afterAll () {
+    public static void afterAll() {
         System.out.println("afterAll");
     }
 
     /* It will trigger before each test */
     @BeforeEach
-    public void beforeEach () {
+    public void beforeEach() {
         System.out.println("beforeEach");
     }
 
     /* It will trigger after each test */
     @AfterEach
-    public void afterEach () {
+    public void afterEach() {
         System.out.println("afterEach");
     }
 
     @Test
-    public void check_default_number_of_books_in_library_is_1_initially () {
+    public void check_default_number_of_books_in_library_is_1_initially() {
 
         String nameOfFirstBook = library.getBooks().get(0).getName();
 
@@ -53,17 +53,17 @@ public class LibraryTest {
     }
 
     @Test
-    public void adding_a_new_book_and_checking_total_books_and_whether_book_is_created () {
+    public void adding_a_new_book_and_checking_total_books_and_whether_book_is_created() {
 
         library.addToCatalogue("Sita", "Devdutt U.", 100, 11.99);
-        
+
         assertEquals(3, totalBooksInLibrary());
 
     }
 
     @Test
-    public void find_an_available_book_to_check_not_null_value () {
-        
+    public void find_an_available_book_to_check_not_null_value() {
+
         Book searchedBook = library.findBookByName("Inner Engineering");
 
         assertNotNull(searchedBook);
@@ -71,8 +71,8 @@ public class LibraryTest {
     }
 
     @Test
-    public void find_an_unavailable_book_to_check_null_value () {
-        
+    public void find_an_unavailable_book_to_check_null_value() {
+
         Book searchedBook = library.findBookByName("Invalid book name");
 
         assertNull(searchedBook);
@@ -80,3 +80,18 @@ public class LibraryTest {
     }
 
 }
+
+/* AAA - arrange, act, assert */
+// class Test {
+//     public void testMethod() {
+//         // Arrange test
+//         Test1 test = new test();
+//         int id;
+
+//         // Act test
+//         id = test.testMethod1();
+
+//         // Assert test
+//         Assert.assertEquals(1, id);
+//     }
+// }
